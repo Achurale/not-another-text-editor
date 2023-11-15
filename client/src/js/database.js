4433b2a0b221983208164d1d8a430b6cf6f8d823
Roymand <chuyirlee@live.com>
2023-11-15 13:24:34 -0600

    All the things

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,47 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try{
+    if (content===null) {
+      console.log('There is nothing to be saved')
+    } else {
+      console.log('Content', content);
+      console.log('Saving to database')
+      const jateDB = await openDB('jate', 1)
+      const tx = jateDB.transaction('jate', 'readwrite')
+      const store = tx.objectStore('jate')
+      const request = store.put({Value: content})
+      const result = await request
+      console.log(`${result} saved to database!`)
+    }
+  } catch {
+    console.error(error)
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    console.log('Grabbing from database');
+    console.log('. . .')
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+
+    if(result.length === 0){
+      console.log("No data has been found");
+    } else{
+    console.log('Retrieved Data', result);
+    return result
+    }
+
+    return result;
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 initdb();
